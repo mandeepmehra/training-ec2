b34ca4192634b1d962c6808339e3d51d56a0614f
Mandeep Mehra <mandeep.mehra@xebia.com>
2021-04-26 23:53:25 -0700

    Converted to vpc module

--- network.tf
+++ network.tf
@@ -1,28 +1,8 @@
-# resource "aws_vpc" "appvpc" {
-#   cidr_block = "10.0.0.0/16"
-#   tags = {
-#     "Name" = "appvpc-mandeep"
-#   }
-# }
-
-data "aws_vpc" "anishvpc" {
-  id = "vpc-0fc666121270d4dfb"
+module "vpcmodule" {
+  source = "./module/vpc"
+  vpc_id = "vpc-0fc666121270d4dfb"
+  subnet_prefix = "mandeep-private"
 }
 
-data "aws_availability_zones" "allazs" {
-  
-}
 
-output "allazs" {
-  value = data.aws_availability_zones.allazs
-}
 
-resource "aws_subnet" "appvpc_subnet" {
-  count = length(data.aws_availability_zones.allazs.names)
-  vpc_id = data.aws_vpc.anishvpc.id
-  cidr_block = "10.0.${11+count.index}.0/28"
-  availability_zone = data.aws_availability_zones.allazs.names[count.index]
-  tags = {
-    "Name" = "appvpc-mandeep-private-10.0.${11 +count.index}.0/28"
-  }
-}
--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
 provider "aws" {
-region = "us-east-1"
+ region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+      bucket = "trainingepsilonbatch4"
+      key = "mandeep/tfstate"
+      region = "us-east-1"
+  }
 }
